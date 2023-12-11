6680e698ad284e4d543764e1f3586d9cc1e359be
Antonio González <antgon@cantab.net>
2023-12-11 17:40:42 +0000

    Updated comments

--- examples/set_time/set_time.c
+++ examples/set_time/set_time.c
@@ -1,7 +1,22 @@
 /*
-Set the time on the DS3231.
+ * Copyright (c) 2023 Antonio González
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
-To set the current time on the DS3231 real-time clock,
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+#include "ds3231.h"
+
+
+/* Set the time on the DS3231.
+
+This helps set the time in the DS3231 real-time clock. If the device has
+a battery backup then it should only be necessary to do this once.
+
+The steps involved are:
 
 (1) After connecting the clock tot the Pico, update the relevant I2C
 definitions below.
@@ -15,14 +30,8 @@ structure. Set a time a few minutes in the future.
 
 (5) Look at a watch or clock: as soon as the time matches the one
 entered below, press `enter`. At that point the date/time will be
-uploaded to the DS3231 and it will start ticking.
+uploaded to the DS3231 and this will start ticking.
 */
-
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/i2c.h"
-
-#include "ds3231.h"
 
 #define DS3231_I2C_PORT i2c1
 #define DS3231_I2C_SDA_PIN 2
@@ -38,19 +47,20 @@ int main() {
 
     ds3231_init(DS3231_I2C_PORT, DS3231_I2C_SDA_PIN,
                 DS3231_I2C_SCL_PIN, &rtc);
-     
+    
+    // Set a date and time here to a few minutes the future.
     ds3231_datetime_t dt = {
-        .year = 2023,
-        .month = 12,
-        .day = 11,
-        .hour = 16,
-        .minutes = 03,
-        .seconds = 00,
-        .dotw = 1,
+        .year = 2023, // Year (2000-2099)
+        .month = 12, // Month (1-12, 1=January)
+        .day = 11, // Day (1-31)
+        .hour = 16, // Hour, 24-h format (0-23)
+        .minutes = 03, // Minutes (0-59)
+        .seconds = 00, // Seconds (0-59)
+        .dotw = 1 // Day of the week (1-7, 1=Monday)
     };
 
     char c;    
-    uint8_t dt_str[25];    
+    uint8_t dt_str[25];
     
     // Wait for a few seconds for the user to open a terminal, then
     // wait for the user to press enter.
